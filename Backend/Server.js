6fdeb454a60b53fdd2b9520001e3ea32659abe0d
eetisahithi3-art <eetisahithi3@gmail.com>
2026-07-23 02:04:10 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,28 +1,47 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 
-app.post("/chat", async (req,res)=>{
+app.post("/chat", (req, res) => {
 
-const message = req.body.message;
-
-
-let reply =
-"Hello! I am your AI Workforce employee. How can I help?";
+    const message = req.body.message.toLowerCase();
+    const company = req.body.company;
 
 
-res.json({
-answer: reply
+    let answer =
+    "Hello! I am your AI Workforce employee. How can I help you?";
+
+
+    if(message.includes("course")){
+        answer =
+        "We provide " + company.services.join(", ");
+    }
+
+
+    else if(message.includes("time")){
+        answer =
+        "Our timings are " + company.timing;
+    }
+
+
+    else if(message.includes("admission")){
+        answer =
+        "Please share your name and phone number. Our team will contact you.";
+    }
+
+
+    res.json({
+        answer: answer
+    });
+
 });
 
-});
 
-
-app.listen(3000,()=>{
-
-console.log("AI Workforce running");
-
+app.listen(3000, () => {
+    console.log("AI Workforce server running");
 });
